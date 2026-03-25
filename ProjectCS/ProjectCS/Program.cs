ead1aa9107c4aa0981cf77b76876a1fe9d5b2894
Jakub Fudala <s33046@pjwstk.edu.pl>
2026-03-25 16:20:03 +0100

    add demo scenario in Program.cs

--- ProjectCS/ProjectCS/Program.cs
+++ ProjectCS/ProjectCS/Program.cs
@@ -1,9 +1,27 @@
+using System;
+using ProjectCS.Models;
+using ProjectCS.Services;
+
 namespace ProjectCS
 {
-    internal class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main()
         {
+            var service = new RentalService();
+
+            var student = new Student(1, "Jakub", "Fudala");
+            var laptop = new Laptop(1, "Dell", 16, "i7");
+            service.RentEquipment(student, laptop, 2);
+            try
+            {
+                service.RentEquipment(student, laptop, 2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Done");
         }
     }
 }
