6275355174bdedd08f096514a07212b168c0d51d
Jakub Fudala <s33046@pjwstk.edu.pl>
2026-03-25 16:16:37 +0100

    add rental model

---
+++ ProjectCS/ProjectCS/Models/Rental.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProjectCS.Models
+{
+    public class Rental
+    {
+        public User User { get; set; }
+        public Equipment Equipment { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public decimal Penalty { get; set; }
+
+        public Rental(User user, Equipment equipment, int days)
+        {
+            User = user;
+            Equipment = equipment;
+            RentDate = DateTime.Now;
+            DueDate = RentDate.AddDays(days);
+        }
+    }
+}
